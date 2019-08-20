06f21e05520255fd6b3aad677aae6619cde1ca3a
bodav <bodavidsen@gmail.com>
2019-08-20 17:58:12 +0200

    added webhook

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 const gpio = require("rpi-gpio");
-const pin = 7;
+const axios = require('axios');
 
+const pin = 7;
 let bellDetected = false;
 let timeoutFunc = null;
-const timeoutReset = 4500;
+const timeoutReset = 5000;
+
+const webhook = "http://localhost:51828/?accessoryId=doorbellSensor&state=true";
 
 gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_RISING, function (err) {
     if (err != undefined) {
@@ -19,13 +22,20 @@ gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_RISING, function (err) {
     });
 });
 
-function gpioChange(channel, val) 
-{
+function gpioChange(channel, val) {
     if (!bellDetected) {
         bellDetected = true;
         console.log("Got GPIO rising edge event");
 
         if (timeoutFunc) clearTimeout(timeoutFunc);
+
+        axios.get(webhook)
+            .then(function (response) {
+                console.log("Webhook response: " + response);
+            })
+            .catch(function (error) {
+                console.log("Webhook error: " + error);
+            });
 
         timeoutFunc = setTimeout(function () {
             console.log("Resetting gpio change event throttle flag");
